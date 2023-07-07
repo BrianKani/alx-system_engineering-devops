568b33688dadd14f4203272d6b61d7fe81ce5fa1
BrianKani <kipsangkangogo26@gmail.com>
2023-07-07 02:21:10 -0700

    16

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -1,2 +1,4 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/^\d{10}$/).join
+#create a Ruby script that accepts one argument and pass it to a regular expression matching method
+
+puts ARGV[0].scan(/^\d{10,10}$/).join

3a9732f3e264cde9a0bfd1db6c0cc8e28d640f51
BrianKani <kipsangkangogo26@gmail.com>
2023-10-10 13:55:05 -0700

    2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -7,16 +7,24 @@ import requests
 import sys
 
 
-def top_ten(subreddit):
+def add_title(hot_list, hot_posts):
+    """ Adds item into a list """
+    if len(hot_posts) == 0:
+        return
+    hot_list.append(hot_posts[0]['data']['title'])
+    hot_posts.pop(0)
+    add_title(hot_list, hot_posts)
+
+
+def recurse(subreddit, hot_list=[], after=None):
     """ Queries to Reddit API """
     u_agent = 'Mozilla/5.0'
-
     headers = {
         'User-Agent': u_agent
     }
 
     params = {
-        'limit': 10
+        'after': after
     }
 
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
@@ -24,13 +32,14 @@ def top_ten(subreddit):
                        headers=headers,
                        params=params,
                        allow_redirects=False)
+
     if res.status_code != 200:
-        print(None)
-        return
+        return None
+
     dic = res.json()
     hot_posts = dic['data']['children']
-    if len(hot_posts) is 0:
-        print(None)
-    else:
-        for post in hot_posts:
-            print(post['data']['title'])
+    add_title(hot_list, hot_posts)
+    after = dic['data']['after']
+    if not after:
+        return hot_list
+    return recurse(subreddit, hot_list=hot_list, after=after)
